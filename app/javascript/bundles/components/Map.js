76ea044517ca84e8db4e140f43a370e7566c9723
RumenRT <33309rt@gmail.com>
2018-10-07 13:44:36 -0400

    got dots to show for favors

--- app/javascript/bundles/components/Map.js
+++ app/javascript/bundles/components/Map.js
@@ -3,12 +3,32 @@ import React, {
 } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-export default class Map extends Component {
+export default class Map extends React.Component {
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
+    this.createMap();
+  }
+
+  createMap = () => {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: `mapbox://styles/mapbox/streets-v9`
+      style: `mapbox://styles/mapbox/streets-v9`,
+      zoom: 12,
+      center: [-80.2044, 25.8028]
+    });
+    const map = this.map;
+    map.on('load', (event) => {
+      map.addSource(
+        'favors', {
+          type: 'geojson',
+          data: `/favors.json`
+        }
+      );
+      map.addLayer({
+        id: 'favors',
+        type: 'circle',
+        source: 'favors'
+      });
     });
   }
 
--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,5 +1,6 @@
 import ReactOnRails from 'react-on-rails'
 import Map from '../bundles/components/Map'
+import 'babel-polyfill'
 
 ReactOnRails.register({
   Map
