80cbcde893bc5330f15419e2db2bed146c1ef141
Dardo Sabarots <Dardojs89@live.com>
2018-10-10 15:49:06 -0400

    Updated map positioning, added Id, cleaned up the div

--- app/javascript/bundles/components/Map.js
+++ app/javascript/bundles/components/Map.js
@@ -7,35 +7,37 @@ import ReactDOMServer from 'react-dom/server'
 import Popup from './Popup'
 
 export default class Map extends React.Component {
-    constructor() {
-      super()
-      this.state = {
-        myFavors: []
-      }
-      window.map = this
+  constructor() {
+    super()
+    this.state = {
+      myFavors: []
     }
-    async componentDidMount() {
-      let data;
-      let favors;
-      const geolocationOptions = {
-        enableHighAccuracy: true,  
-        maximumAge: 30000,
-        timeout: 27000
-      } 
-      // options for mapbox comp
-      const mapOptions = {
-        container: this.mapContainer,
-        style: `mapbox://styles/mapbox/streets-v9`,
-        zoom: 12,
-        center: [-80.2044, 25.8028]
-      }
+    window.map = this
+  }
+  async componentDidMount() {
+    let data;
+    let favors;
+    const geolocationOptions = {
+      enableHighAccuracy: true,
+      maximumAge: 30000,
+      timeout: 27000
+    }
+    // options for mapbox comp
+    const mapOptions = {
+      container: this.mapContainer,
+      style: `mapbox://styles/mapbox/streets-v9`,
+      pitch: 40, // pitch in degrees
+      bearing: -1, // bearing in degrees
+      zoom: 15,
+      center: [-80.2044, 25.8028]
+    }
 
-      data = await axios.get('/favors.json')
-      favors = data.data
-      this.setState({myFavors: favors})
-      mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
-      await this.createMap(mapOptions, geolocationOptions);
-    }
+    data = await axios.get('/favors.json')
+    favors = data.data
+    this.setState({ myFavors: favors })
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
+    await this.createMap(mapOptions, geolocationOptions);
+  }
 
   // initializes maps
   createMap = (mapOptions, geolocationOptions) => {
@@ -43,7 +45,7 @@ export default class Map extends React.Component {
     const map = this.map;
 
     // appends geolocator button
-    map.addControl( 
+    map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: geolocationOptions,
         trackUserLocation: true
@@ -68,21 +70,21 @@ export default class Map extends React.Component {
     const map = this.map;
     const self = this;
     await axios.get(`/favors.json`)
-    .then((res) => {
-      let newMarkers = res.data 
-      newMarkers.features.forEach((favor, i) => {
-        var elm = document.createElement('div')
-        elm.className = `${favor.properties.category}`
-        let popup = new mapboxgl.Popup({ offset: 25 })
-        .setHTML(ReactDOMServer.renderToStaticMarkup(
-          <Popup favor={favor.properties}></Popup>
-        ))
-        let marker = new mapboxgl.Marker(elm)
-        .setLngLat(favor.geometry.coordinates)
-        .setPopup(popup);
-        marker.addTo(map)
+      .then((res) => {
+        let newMarkers = res.data
+        newMarkers.features.forEach((favor, i) => {
+          var elm = document.createElement('div')
+          elm.className = `${favor.properties.category}`
+          let popup = new mapboxgl.Popup({ offset: 25 })
+            .setHTML(ReactDOMServer.renderToStaticMarkup(
+              <Popup favor={favor.properties}></Popup>
+            ))
+          let marker = new mapboxgl.Marker(elm)
+            .setLngLat(favor.geometry.coordinates)
+            .setPopup(popup);
+          marker.addTo(map)
+        })
       })
-    })
   }
 
   render() {
@@ -91,12 +93,12 @@ export default class Map extends React.Component {
       height: '650px',
       backgroundColor: 'azure'
     };
-    return <div style = {
+    return <div style={
       style
     }
-    ref = {
-      el => this.mapContainer = el
-    }
+      ref={
+        el => this.mapContainer = el
+      }
     />;
   }
 
