d7c7a9f7c41ecbe160033f4deed096b715432c72
Dardo Sabarots <Dardojs89@live.com>
2018-10-11 12:34:17 -0400

    cleaned up Popup and styling

--- app/javascript/bundles/components/Popup.js
+++ app/javascript/bundles/components/Popup.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 export default class Popup extends Component {
 
   render() {
     return (
       <div className="map-popup" key={this.props.favor.id}>
-        <p><strong>{this.props.favor.category}</strong></p>
+        <p id="categorypopup"><strong>{this.props.favor.category}</strong></p>
         <p><strong>{this.props.favor.description}</strong></p>
         <p><strong>{this.props.favor.address}</strong></p>
       </div>
