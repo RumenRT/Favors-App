3ecb94768047f1d204093d40a60f99d9fcaa4226
Ariel404 <arielrubin13@gmail.com>
2018-10-11 17:19:41 -0400

    created basic table for Favor near you

--- app/controllers/favor_browser_controller.rb
+++ app/controllers/favor_browser_controller.rb
@@ -1,8 +1,8 @@
 class FavorBrowserController < ApplicationController
     def index
-        @foo = "hello world"
-        @favor = Favor.all
-        @methods = @favor.methods.reject{|m| m.to_s.include? 'a'}
+        
+        @favors = Favor.all
+       
     end
 
 end
---
+++ app/javascript/packs/App.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails'
+import Map from '../bundles/components/Map'
+import 'babel-polyfill'
+
+ReactOnRails.register({
+  Map
+})
