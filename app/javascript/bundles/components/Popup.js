3d010769a6c72fe428e9fb58b570c6d9c672efad
Dardo Sabarots <Dardojs89@live.com>
2018-10-14 04:25:01 -0400

    Fixed styling, corrected titles, now drawing right info

--- app/javascript/bundles/components/Popup.js
+++ app/javascript/bundles/components/Popup.js
@@ -5,10 +5,10 @@ export default class Popup extends Component {
   render() {
     return (
       <div className="map-popup" key={this.props.favor.id}>
-        <p><strong>Created By: {this.props.favor.user_name}</strong></p>
         <p id="categorypopup"><strong>{this.props.favor.category}</strong></p>
-        <p><strong>{this.props.favor.description}</strong></p>
-        <p><strong>{this.props.favor.address}</strong></p>
+        <p><strong>Name: {this.props.favor.user_name}</strong></p>
+        <p><strong>Needs: {this.props.favor.description}</strong></p>
+        <p id="popupaddress"><strong>{this.props.favor.address}</strong></p>
         <a href={`/favors/${this.props.favor.id}/claim?user_id=${this.props.user_id}`}>Claim Favor</a>
       </div >
     )
