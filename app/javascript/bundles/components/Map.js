fff60ca096a847a0f93550e10712a596f8207bbb
Dardo Sabarots <Dardojs89@live.com>
2018-10-16 13:49:33 -0400

    Ready for mobile view presentation for pitchday

--- app/javascript/bundles/components/Map.js
+++ app/javascript/bundles/components/Map.js
@@ -25,9 +25,9 @@ export default class Map extends Component {
     const mapOptions = {
       container: this.mapContainer,
       style: `mapbox://styles/mapbox/streets-v9`,
-      pitch: 16, // pitch in degrees
-      bearing: 1, // bearing in degrees
-      zoom: 14,
+      pitch: 0, // pitch in degrees
+      bearing: 0, // bearing in degrees
+      zoom: 15,
       minZoom: 5,
       maxZoom: 25,
       center: [-80.2044, 25.8028]
@@ -159,40 +159,4 @@ export default class Map extends Component {
   componentWillUnmount() {
     this.map.remove();
   }
-}
-
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-    e.preventDefault();
-  e.returnValue = false;
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-function disableScroll() {
-  if (window.addEventListener) // older FF
-    window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove = preventDefault; // mobile
-  document.onkeydown = preventDefaultForScrollKeys;
-}
-
-function enableScroll() {
-  if (window.removeEventListener)
-    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.onmousewheel = document.onmousewheel = null;
-  window.onwheel = null;
-  window.ontouchmove = null;
-  document.onkeydown = null;
 }
