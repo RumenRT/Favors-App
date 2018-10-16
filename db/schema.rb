0a0734ae7f0c8ff0b6e98b2445f10df2386f755b
Dardo Sabarots <Dardojs89@live.com>
2018-10-16 04:54:15 -0400

    Added Notices upon task creation or deletion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_203917) do
+ActiveRecord::Schema.define(version: 2018_10_15_231618) do
 
   create_table "favors", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2018_10_14_203917) do
     t.integer "performer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "read_status", default: false
     t.index ["favor_id"], name: "index_notifications_on_favor_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
